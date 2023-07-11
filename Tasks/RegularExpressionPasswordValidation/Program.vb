ee7b4e6c6d70fa550493b59141fae5d6f04d23bb
miarnab <mitraarnab.2013@gmail.com>
2023-07-11 15:11:40 +0530

    Adding Chnages

--- Tasks/RegularExpressionPasswordValidation/Program.vb
+++ Tasks/RegularExpressionPasswordValidation/Program.vb
@@ -1,7 +1,23 @@
 Imports System
+Imports System.Text.RegularExpressions
 
 Module Program
+    Function PasswordCheck(pass As String)
+        Dim pattern As String = "^.*(?=.{8,15})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$"
+        If Regex.Match(pass, pattern).Success Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Enter the password: ")
+        Dim password As String = Console.ReadLine()
+        If PasswordCheck(password) = True Then
+            Console.WriteLine("Password is valid")
+        Else
+            Console.WriteLine("Password is invalid")
+
+        End If
     End Sub
 End Module
